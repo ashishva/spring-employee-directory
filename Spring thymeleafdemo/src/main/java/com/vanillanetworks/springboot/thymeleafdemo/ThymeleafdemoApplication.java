e9c8305555f9a485a4ccc97c5b87b35daee25f80
Ashish Augustine <ashish.augustine123@gmail.com>
2022-06-16 13:43:57 +0200

    Spring thymeleaf demo

--- Spring thymeleafdemo/src/main/java/com/vanillanetworks/springboot/thymeleafdemo/ThymeleafdemoApplication.java
+++ Spring thymeleafdemo/src/main/java/com/vanillanetworks/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -9,6 +9,6 @@ public class ThymeleafdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafdemoApplication.class, args);
 	}
-
+ 
 }      //http://localhost:8080/employees/list
 
